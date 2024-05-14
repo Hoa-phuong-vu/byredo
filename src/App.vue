62d943744e7381c98d7990bbf231fa4c7e0c1a9e
Hoa Vu <hoavu060404@gmail.com>
2024-05-14 19:17:24 +1000

    images

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
   <nav>
+    <router-link to="/"><img src="./assets/logo.svg"></router-link>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">Products</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+body {
+  background-color: #F5F4F2;
+  color: black;
+  font-family: 'Boska', serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Boska', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+  padding: 10px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: black;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #EBE9E6;
+}
+
+nav img {
+  width: 125px;
+  display: block;
 }
 </style>

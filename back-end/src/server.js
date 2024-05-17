8b573adf4758ccaa3c9ffc997fa45c8b0d11fd27
Hoa Vu <hoavu060404@gmail.com>
2024-05-17 22:53:33 +1000

    load data from express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,1 +1,27 @@
+ import express from "express";
+ import { cartItems, products } from "./temp-data";
+
+ const app = express();
  
+app.get('/hello', (req, res) => {
+    res.send('hello!');
+});
+
+
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/cart', (req, res) => {
+    res.json(cartItems);
+});
+
+app.get('/products/:productId', (req, res) => {
+    const productId = req.params.productId;
+    const product = products.find(product => product.id === productId);
+    res.json(product);
+});
+
+ app.listen(8000, () => {
+    console.log('Server is listening on port 8000')
+ });
---
+++ back-end/src/temp-data.js
@@ -0,0 +1,59 @@
+// import blanche from './assets/Blanche.jpg';
+// import gypsy from './assets/gypsy.jpg';
+// import mojave from './assets/Mojave.jpg';
+// import rose from './assets/rose.jpg';
+// import bal from './assets/Bal.jpg';
+
+export const cartItems = [
+  {
+    id: '1',
+    name: "Bal d'Afrique",
+    price: '$300',
+    // image: bal,
+  },
+  {
+    id: '2',
+    name: "Blanche",
+    price: '$303',
+    // image: blanche,
+  },
+  {
+    id: '3',
+    name: "Gypsy Water",
+    price: '$301',
+    // image: gypsy,
+  }
+];
+
+export const products = [
+  {
+    id: '1',
+    name: "Bal d'Afrique",
+    price: '$300',
+    // image: bal,
+  },
+  {
+    id: '2',
+    name: "Blanche",
+    price: '$303',
+    // image: blanche,
+  },
+  {
+    id: '3',
+    name: "Gypsy Water",
+    price: '$301',
+    // image: gypsy,
+  },
+  {
+    id: '4',
+    name: "Mojave Ghost",
+    price: '$305',
+    // image: mojave,
+  },
+  {
+    id: '5',
+    name: "Rose Of No Man's Land",
+    price: '$306',
+    // image: rose,
+  }
+];

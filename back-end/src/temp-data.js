ff6446c5338a995996596aadada2ef4638c9fd73
Hoa Vu <hoavu060404@gmail.com>
2024-05-20 18:08:55 +1000

    Url changed

--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -1,8 +1,4 @@
-// import blanche from './assets/Blanche.jpg';
-// import gypsy from './assets/gypsy.jpg';
-// import mojave from './assets/Mojave.jpg';
-// import rose from './assets/rose.jpg';
-// import bal from './assets/Bal.jpg';
+
 
 export const cartItems = [
   '1', '2'
@@ -13,30 +9,30 @@ export const products = [
     id: '1',
     name: "Bal d'Afrique",
     price: '$300',
-    // image: bal,
+    imageUrl: '/images/Bal.jpg',
   },
   {
     id: '2',
     name: "Blanche",
     price: '$303',
-    // image: blanche,
+    imageUrl: '/images/blanche.jpg',
   },
   {
     id: '3',
     name: "Gypsy Water",
     price: '$301',
-    // image: gypsy,
+    imageUrl: '/images/gypsy.jpg',
   },
   {
     id: '4',
     name: "Mojave Ghost",
     price: '$305',
-    // image: mojave,
+    imageUrl: '/images/mojave.jpg',
   },
   {
     id: '5',
     name: "Rose Of No Man's Land",
     price: '$306',
-    // image: rose,
+    imageUrl: '/images/rose.jpg',
   }
 ];

2a7d3087e271bc10ece48f2877ecdaf8919c2609
Hoa Vu <hoavu060404@gmail.com>
2024-05-20 19:39:58 +1000

    changing for mongodb

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,56 +1,60 @@
 import express from 'express';
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
 
 let cartItems = cartItemsRaw;
 let products = productsRaw;
 
-const url = `mongodb+srv://vuphuonghoa:vuphuonghoa@cluster0.dd8seao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; 
-const client = new MongoClient(url)
 
-const app = express();
-app.use(express.json());
 
-app.get('/hello', async(req, res) => {
+async function start() {
+  const url = `mongodb+srv://vuphuonghoa:vuphuonghoa@cluster0.dd8seao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  const client = new MongoClient(url);
+  
   await client.connect();
   const db = client.db('byredo-db');
-  const products = await db.collection('products').find({}).toArray();
-  res.send(products);
-});
 
-app.get('/products', (req, res) => {
-  res.json(products);
-});
+  const app = express();
+  app.use(express.json());
 
-function populateCartIds(ids) {
-  return ids.map(id => products.find(product => product.id === id));
+  app.get('/products', async (req, res) => {
+    const products = await db.collection('products').find({}).toArray();
+    res.send(products);
+  });
+
+  async function populateCartIds(ids) {
+    return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
+  }
+
+  app.get('/users/:userId/cart', async (req, res) => {
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populateCartIds(user.cartItems);
+    res.json(populatedCart);
+  });
+
+  app.get('/products/:productId', async(req, res) => {
+    const productId = req.params.productId;
+    const product = await db.collection('products').findOne({ id: productId });
+    res.json(product);
+  });
+
+  app.post('/cart', (req, res) => {
+    const productId = req.body.id;
+    cartItems.push(productId);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
+  });
+
+  app.delete('/cart/:productId', (req, res) => {
+    const productId = req.params.productId;
+    cartItems = cartItems.filter(id => id !== productId);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
+  });
+
+  app.listen(8000, () => {
+    console.log('Server is listening on port 8000')
+  });
 }
 
-app.get('/cart', (req, res) => {
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.get('/products/:productId', (req, res) => {
-  const productId = req.params.productId;
-  const product = products.find(product => product.id === productId);
-  res.json(product);
-});
-
-app.post('/cart', (req, res) => {
-  const productId = req.body.id;
-  cartItems.push(productId);
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.delete('/cart/:productId', (req, res) => {
-  const productId = req.params.productId;
-  cartItems = cartItems.filter(id => id !== productId);
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000')
-});
+start();

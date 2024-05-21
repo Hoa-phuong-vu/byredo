26853fad4479d8ec2d9fdfe19bd139c1e91940a0
Hoa Vu <hoavu060404@gmail.com>
2024-05-21 20:01:30 +1000

    cart total finished

--- front-end/src/components/ProductsList.vue
+++ front-end/src/components/ProductsList.vue
@@ -8,17 +8,12 @@
             <img :src="product.imageUrl" :alt="product.name"/>
             <h3 class="product-name">{{ product.name}}</h3>
             <p class="product-price"> {{ product.price}}</p>
-<<<<<<< Updated upstream
             <router-link :to="'/products/' + product.id">
             <button>View Details</button>
             </router-link>
-=======
-        
-            </div>
-        </router-link>
->>>>>>> Stashed changes
         </div>
-    </div>
+        </div>
+
 </template>
 
 <script>

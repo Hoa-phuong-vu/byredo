55b010598b81d161fee9718b7759865962e084ad
Hoa Vu <hoavu060404@gmail.com>
2024-06-03 09:08:14 +0700

    final changes

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,19 +3,4 @@ import App from './App.vue'
 import router from './router'
 import './main.css';
 
-
-// import { initializeApp } from "firebase/app";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDFOwR4Qo2VBmaqETxnKK4g8vlp9r3N8bo",
-//   authDomain: "byredo-1599d.firebaseapp.com",
-//   projectId: "byredo-1599d",
-//   storageBucket: "byredo-1599d.appspot.com",
-//   messagingSenderId: "834180545775",
-//   appId: "1:834180545775:web:d7dc6278b97e69b679ef1a"
-// };
-
-// initializeApp(firebaseConfig);
-
-
 createApp(App).use(router).mount('#app')
--- front-end/src/views/FavView.vue
+++ front-end/src/views/FavView.vue
@@ -1,56 +1,66 @@
 <template>
-    <div>
-      <button v-if="isLoggedIn" @click="logout">Sign Out</button>
-      <h1>Favourites</h1>
-      <div v-if="isLoggedIn && favItems.length > 0">
-        <FavProducts @remove-from-fav="removeFromFav" :favItems="favItems" />
-      </div>
-      <div v-else-if="isLoggedIn && favItems.length === 0">
-        You currently have no items in your Favourites!
-      </div>
-      <div v-else>
-        Please <router-link to="/login">log in</router-link> to view your favourites.
-      </div>
+  <div>
+    <button v-if="isLoggedIn" @click="logout">Sign Out</button>
+    <button v-if="isLoggedIn && favItems.length > 0" @click="clearFavourites">Clear Favourites</button>
+    <h1>Favourites</h1>
+    <div v-if="isLoggedIn && favItems.length > 0">
+      <FavProducts @remove-from-fav="removeFromFav" :favItems="favItems" />
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import FavProducts from '@/components/FavProducts.vue';
-  
-  export default {
-    name: 'FavouritesPage',
-    components: {
-      FavProducts,
-    },
-    data() {
-      return {
-        isLoggedIn: false,
-        favItems: [],
+    <div v-else-if="isLoggedIn && favItems.length === 0">
+      You currently have no items in your Favourites!
+    </div>
+    <div v-else>
+      Please <router-link to="/login">log in</router-link> to view your favourites.
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import FavProducts from '@/components/FavProducts.vue';
+
+export default {
+  name: 'FavouritesPage',
+  components: {
+    FavProducts,
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+      favItems: [], // Ensure favItems is initialized
+    }
+  },
+  methods: {
+    async removeFromFav(productId) {
+      try {
+        const response = await axios.delete(`/api/users/103837395/fav/${productId}`);
+        this.favItems = response.data;
+      } catch (error) {
+        console.error('Error removing item from favourites:', error);
       }
     },
-    methods: {
-      async removeFromFav(productId) {
-        const response = await axios.delete(`/api/users/103837395/fav/${productId}`);
-        const updatedCart = response.data;
-        this.favItems = updatedCart;
-      },
-      async logout() {
-        localStorage.removeItem('isLoggedIn'); // Remove user authentication status
-        this.isLoggedIn = false;
-        this.$router.push('/login');
-      },
-      async fetchFavourites() {
+    async logout() {
+      localStorage.removeItem('isLoggedIn'); // Remove user authentication status
+      this.isLoggedIn = false;
+      this.$router.push('/login');
+    },
+    async fetchFavourites() {
+      try {
         const response = await axios.get('/api/users/103837395/fav');
         this.favItems = response.data;
+      } catch (error) {
+        console.error('Error fetching favourites:', error);
       }
     },
-    async created() {
-      this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'; // Check if user is logged in
-      if (this.isLoggedIn) {
-        await this.fetchFavourites();
-      }
+    clearFavourites() {
+      this.favItems = []; // Reset the favItems array to an empty array
+    }
+  },
+  async created() {
+    this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'; // Check if user is logged in
+    if (this.isLoggedIn) {
+      await this.fetchFavourites();
     }
   }
-  </script>
-  
+}
+</script>
